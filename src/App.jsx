fb831c362ddcf66e4035b43bb04031efe597791a
RitaBB <ritaballesterosba@gmail.com>
2024-07-02 06:44:14 -0500

    routes dom

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,23 @@ import { Animal3 } from './components/Animal3'
 import { Animal4 } from './components/Animal4'
 import { Animal5 } from './components/Animal5'
 import { Animal6 } from './components/Animal6'
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
 
 
   return (
     <>
-
-    <Animal1 />
-    <Animal2 />
-    <Animal3 />
-    <Animal4 />
-    <Animal5 />
-    <Animal6 />
-    
-
+    <BrowserRouter>
+      <Routes>
+        <Route path="/zorro"    element={<Animal1 /> } />
+        <Route path="/leopardo" element={<Animal2 /> } />
+        <Route path="/mapache"  element={<Animal3 /> } />
+        <Route path="/gato"     element={<Animal4 /> } />
+        <Route path="/hamster"  element={<Animal5 /> } />
+        <Route path="/colibri"  element={<Animal6 /> } />
+      </Routes>
+    </BrowserRouter>
     
     </>
   )
